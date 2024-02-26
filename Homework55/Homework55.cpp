e48d55b28c56e0bdc5de0715d2a985a3c1de7cc6
Sasha Novak <Xamilio25@gmail.com>
2024-02-26 17:47:26 +0200

    Добавьте файлы проекта.

---
+++ Homework55/Homework55.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class TextCompressor {
+private:
+    string text;
+public:
+    TextCompressor(const string& inputText)
+    {
+        text = inputText;
+    }
+    string compText() {
+        string compText = "";
+        bool fWord = true;
+
+        for (char ch : text) {
+            if (ch != ' ') {
+                if (fWord) 
+                {
+                    compText += ch;
+                    fWord = false;
+                }
+                else 
+                {
+                    if (compText.back() != ' ')
+                    {
+                        compText += ' ';
+                    }
+                    compText += ch;
+                }
+            }
+        }
+        return compText;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    string inputText = "Это      текст   для    сжатия    ";
+
+    TextCompressor comp(inputText);
+    string compText = comp.compText();
+
+    cout << compText << endl;
+}
